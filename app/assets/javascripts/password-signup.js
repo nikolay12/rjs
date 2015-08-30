89f3e56941cdbd1563d8b7f0d15edc0a90b9553b
Nikolay Nikolov <nikolay12@gmail.com>
2015-08-30 23:18:11 +0100

    Just adding the js file

---
+++ app/assets/javascripts/password-signup.js
@@ -0,0 +1,38 @@
+/**
+ * Created by nnikolo on 30/08/15.
+ */
+(function () {
+    var password = document.getElementById('user_password'); // Store password inputs
+    var passwordConfirm = document.getElementById('user_password_confirmation');
+
+    function setErrorHighlighter(e) {
+        var target = e.target || e.srcElement;             // Get target element
+        if (target.value.length < 8) {                     // If its length is < 8
+            target.className = 'fail';                       // Set class to fail
+        } else {                                           // Otherwise
+            target.className = 'pass';                       // Set class to pass
+        }
+    }
+
+    function removeErrorHighlighter(e) {
+        var target = e.target || e.srcElement;              // Get target element
+        if (target.className === 'fail') {                  // If class shows fail
+            target.className = '';                            // Clear class
+        }
+    }
+
+    function passwordsMatch(e) {
+        var target = e.target || e.srcElement;               // Get target element
+        // If value matches pwd and it is longer than 8 characters
+        if ((password.value === target.value) && target.value.length >= 8) {
+            target.className = 'pass';                         // Set class to pass
+        } else {                                             // Otherwise
+            target.className = 'fail';                         // Set class to fail
+        }
+    }
+
+    addEvent(password, 'focus', removeErrorHighlighter);
+    addEvent(password, 'blur', setErrorHighlighter);
+    addEvent(passwordConfirm, 'focus', removeErrorHighlighter);
+    addEvent(passwordConfirm, 'blur', passwordsMatch);
+}());
